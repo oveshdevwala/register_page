d04c413fd24b7848c31e0f4efa64329d751c3b5a
oveshdevwala <oveshshaikh790@gmail.com>
2024-05-23 13:05:06 +0530

    error fixed

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,15 @@ import 'package:job_asignment_1_ovesh/core/theme/theme_data/app_theme.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => RegisterProvider()),
+        ChangeNotifierProvider(create: (context) => LandingPageProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -19,16 +27,9 @@ class MyApp extends StatelessWidget {
       designSize: MediaQuery.of(context).size,
       builder: (_, child) {
         return MaterialApp(
-          theme: AppTheme.lightThemeData,
-          debugShowCheckedModeBanner: false,
-          home: MultiProvider(
-            providers: [
-              ChangeNotifierProvider(create: (context) => RegisterProvider()),
-              ChangeNotifierProvider(create: (context) => LandingPageProvider()),
-            ],
-            child: const RegisterPage(),
-          ),
-        );
+            theme: AppTheme.lightThemeData,
+            debugShowCheckedModeBanner: false,
+            home: const RegisterPage());
       },
     );
   }
